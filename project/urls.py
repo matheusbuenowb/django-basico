37754dbb1639d8843996e78ebc036112d5f4b49b
Matheus Bueno <matheus.faria_wb@hotmail.com>
2025-09-19 17:41:09 -0300

    Transferido a responsabilidade de views para os dir home e blog

--- project/urls.py
+++ project/urls.py
@@ -16,18 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
+from home import views as home_views
+from blog import views as blog_views
 
 #Model, view, controller
 
-def blog(request):  ##view
-    return HttpResponse('blog')
-
-def home(request): ##view
-    return HttpResponse('Home')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', blog),
-    path('', home)
+    path('blog/', blog_views.blog),
+    path('', home_views.home)
 ]
